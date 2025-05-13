e69ea3c8da23db4cbd64d0fd224fd05ebe056bf0
brahim17A <bzerkouk3@gmail.com>
2025-05-13 23:31:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-function playClick() {
-  const audio = document.getElementById('clickSound');
-  audio.volume = 0.2;
-  audio.play();
-}
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#0ff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#0ff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      }
+    }
+  },
+  "retina_detect": true
+});
